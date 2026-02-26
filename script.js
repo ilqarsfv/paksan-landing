c01557b65c1c65d85316b881bb90d2d0678f3816
Ilqar <ilqar.sofiyev.95@gmail.com>
2026-02-26 15:22:55 +0400

    v3.6

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 $(document).ready(() => {
   var swiper = new Swiper(".productSwiper", {
     lazy: true,
-    slidesPerView: 3,
-    spaceBetween: 30,
     speed: 1000,
     loop: true,
+    spaceBetween: 20,
     autoplay: {
       delay: 2500,
       disableOnInteraction: false,
     },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
   });
 
   $(".faq_wrap").click(function () {
-    $(this).find("button").toggleClass("active")
+    $(this).find("button").toggleClass("active");
     $(this).find(".faq_content").slideToggle();
+  });
+
+  // mob menu
+  $("#get_mob_menu, #close_mob_menu").click(() => {
+    $("#mob_menu").toggleClass("active");
   });
 });
